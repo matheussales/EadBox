1cfb686988bd9eb58757214b378c53c3de22a735
Matheus Martins de Sales <m.martins.sales@hotmail.com>
2018-08-14 22:23:25 -0300

    Detalhe do Curso
    Refatoração do codigo da tela de Detalhe do Curso.

--- app/src/main/java/com/example/mmart/eadbox/ui/activity/CourseDetailActivity.kt
+++ app/src/main/java/com/example/mmart/eadbox/ui/activity/CourseDetailActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.example.mmart.eadbox.R
 import com.example.mmart.eadbox.model.Course
+import com.example.mmart.eadbox.model.Lecture
 import com.example.mmart.eadbox.ui.adapter.ListLectureAdapter
 import kotlinx.android.synthetic.main.activity_course_detail.*
 import java.net.URL
@@ -16,18 +17,35 @@ class CourseDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_detail)
 
-        val course: Course = intent.getSerializableExtra("course") as Course
+        val course: Course = findCourse()
 
-        val lectures = course.lectures
+        configureTitle(course)
+        setImageBanner(course)
+        val lectures = findLectures(course)
 
-        title = course.title
-        val url = URL(course.logo)
-        val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+        configureAdapter(lectures)
+    }
 
-        course_logo.setImageBitmap(bitmap)
+    private fun findLectures(course: Course): List<Lecture> {
+        return course.lectures
+    }
 
+    private fun configureAdapter(lectures: List<Lecture>) {
         lecture_recycler_view.adapter = ListLectureAdapter(this, lectures)
         lecture_recycler_view.layoutManager = LinearLayoutManager(this)
+    }
 
+    private fun setImageBanner(course: Course) {
+        val url = URL(course.logo)
+        val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+        course_logo.setImageBitmap(bitmap)
+    }
+
+    private fun findCourse(): Course {
+        return intent.getSerializableExtra("course") as Course
+    }
+
+    private fun configureTitle(course: Course) {
+        title = course.title
     }
 }
